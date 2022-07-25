f3188c16129180676da0ce8edb1d79ad2c8a7d61
Mark Bonicillo <mark.bonicillo@pnnl.gov>
2022-07-25 11:44:52 -0700

    Add simple unit test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,40 @@
 """Tests suite for `platform_driver_agent`."""
-
 from pathlib import Path
+
+import pytest
+
+from volttron.driver.base.interfaces import BaseInterface
 
 TESTS_DIR = Path(__file__).parent
 TMP_DIR = TESTS_DIR / "tmp"
 FIXTURES_DIR = TESTS_DIR / "fixtures"
 """Configuration for the pytest test suite."""
+
+
+@pytest.fixture()
+def generic_test_interface():
+
+    class GenericTestInterface(BaseInterface):
+
+        def __init__(self, **kwargs):
+            super(GenericTestInterface, self).__init__(**kwargs)
+
+        def configure(self, config_dict, registry_config_str):
+            return
+
+        def get_point(self, point_name, get_priority_array=False):
+            return
+
+        def set_point(self, point_name, value, priority=None):
+            return
+
+        def scrape_all(self):
+            return
+
+        def revert_all(self, priority=None):
+            return
+
+        def revert_point(self, point_name, priority=None):
+            return
+
+    yield GenericTestInterface()
--- tests/test_driver.py
+++ tests/test_driver.py
@@ -1,7 +1,6 @@
-"""Unit tests for agent"""
-
-import pytest
+"""Unit tests for base driver"""
 
 
-def test_driver():
-    pass
+def test_base_interface_should_have_empty_point_map(generic_test_interface):
+
+    assert not generic_test_interface.get_register_names()
