dc583e181a1250e5a10591fcc8ea519c79c7f189
David M. Raker <david.raker@pnnl.gov>
2025-11-21 08:37:50 -0800

    Made PointConfig and Equipment config handle empty strings as default value. Necessary to correctly handle common issue with CSV.

--- src/volttron/driver/base/interfaces.py
+++ src/volttron/driver/base/interfaces.py
@@ -246,6 +246,7 @@ class BaseInterface(object, metaclass=abc.ABCMeta):
 
     REGISTER_CONFIG_CLASS = PointConfig
     INTERFACE_CONFIG_CLASS = RemoteConfig
+    default_config = None
 
     def __init__(self, config: RemoteConfig, driver_agent, *args, **kwargs):
         # Object does not take any arguments to the init.
